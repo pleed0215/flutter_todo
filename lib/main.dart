429f011432fdcfba4eb28599d63ca18d3e29b81f
Lee Eun Deok <pleed0215@gmail.com>
2021-09-28 17:50:27 +0900

    Animationnnnn

--- lib/main.dart
+++ lib/main.dart
@@ -53,6 +53,66 @@ class _AnimationAppState extends State<AnimationApp> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+        appBar: AppBar(title: const Text('Animation Example')),
+        body: Center(
+          child: Column(
+            children: [
+              SizedBox(
+                  child: Row(
+                      children: [
+                        SizedBox(
+                            width: 100,
+                            child: Text('이름: ${peoples[current].name}')),
+                        AnimatedContainer(
+                            duration: const Duration(seconds: 2),
+                            curve: Curves.bounceIn,
+                            color: Colors.amber,
+                            child: Text('키 ${peoples[current].height}',
+                                textAlign: TextAlign.center),
+                            width: 50,
+                            height: peoples[current].height),
+                        AnimatedContainer(
+                            duration: const Duration(seconds: 2),
+                            curve: Curves.easeInCubic,
+                            color: Colors.blue,
+                            child: Text('몸무게 ${peoples[current].weight}',
+                                textAlign: TextAlign.center),
+                            width: 50,
+                            height: peoples[current].weight),
+                        AnimatedContainer(
+                            duration: const Duration(seconds: 2),
+                            curve: Curves.linear,
+                            color: Colors.pinkAccent,
+                            child: Text('BMI ${peoples[current].bmi}',
+                                textAlign: TextAlign.center),
+                            width: 50,
+                            height: peoples[current].bmi),
+                      ],
+                      mainAxisAlignment: MainAxisAlignment.spaceAround,
+                      crossAxisAlignment: CrossAxisAlignment.end),
+                  height: 200),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      if (current < peoples.length - 1) {
+                        current++;
+                      }
+                    });
+                  },
+                  child: const Text("다음")),
+              ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      if (current > 0) {
+                        current--;
+                      }
+                    });
+                  },
+                  child: const Text("이전"))
+            ],
+            mainAxisAlignment: MainAxisAlignment.center,
+          ),
+        ));
   }
 }
