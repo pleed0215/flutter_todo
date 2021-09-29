3427ef37520c7f8497c02a50204e5ed11c6c5315
Lee Eun Deok <pleed0215@gmail.com>
2021-09-29 13:00:26 +0900

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_todo/people.dart';
-import 'package:flutter_todo/second_page.dart';
-import 'package:flutter_todo/sliver_page.dart';
+
 
 
 void main() {
@@ -28,109 +26,22 @@ class MyApp extends StatelessWidget {
           // is not restarted.
           primarySwatch: Colors.blue,
         ),
-        home: const AnimationApp());
+        home: const MyHome());
   }
 }
 
-class AnimationApp extends StatefulWidget {
-  const AnimationApp({Key? key}) : super(key: key);
+class MyHome extends StatefulWidget {
+  const MyHome({ Key? key }) : super(key: key);
 
   @override
-  _AnimationAppState createState() => _AnimationAppState();
+  _MyHomeState createState() => _MyHomeState();
 }
 
-class _AnimationAppState extends State<AnimationApp> {
-  List<People> peoples = List.empty(growable: true);
-  int current = 0;
-
-  @override
-  void initState() {
-    super.initState();
-    peoples.add(People(name: '스미스', height: 180, weight: 92));
-    peoples.add(People(name: '메리', height: 162, weight: 55));
-    peoples.add(People(name: '존', height: 177, weight: 75));
-    peoples.add(People(name: '바트', height: 130, weight: 40));
-    peoples.add(People(name: '콘', height: 194, weight: 140));
-    peoples.add(People(name: '디디', height: 100, weight: 80));
-  }
-
+class _MyHomeState extends State<MyHome> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: const Text('Animation Example')),
-        body: Center(
-          child: Column(
-            children: [
-              SizedBox(
-                  child: Row(
-                      children: [
-                        SizedBox(
-                            width: 100,
-                            child: Text('이름: ${peoples[current].name}')),
-                        AnimatedContainer(
-                            duration: const Duration(seconds: 2),
-                            curve: Curves.bounceIn,
-                            color: Colors.amber,
-                            child: Text('키 ${peoples[current].height}',
-                                textAlign: TextAlign.center),
-                            width: 50,
-                            height: peoples[current].height),
-                        AnimatedContainer(
-                            duration: const Duration(seconds: 2),
-                            curve: Curves.easeInCubic,
-                            color: Colors.blue,
-                            child: Text('몸무게 ${peoples[current].weight}',
-                                textAlign: TextAlign.center),
-                            width: 50,
-                            height: peoples[current].weight),
-                        AnimatedContainer(
-                            duration: const Duration(seconds: 2),
-                            curve: Curves.linear,
-                            color: Colors.pinkAccent,
-                            child: Text('BMI ${peoples[current].bmi}',
-                                textAlign: TextAlign.center),
-                            width: 50,
-                            height: peoples[current].bmi),
-                      ],
-                      mainAxisAlignment: MainAxisAlignment.spaceAround,
-                      crossAxisAlignment: CrossAxisAlignment.end),
-                  height: 200),
-              ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      if (current < peoples.length - 1) {
-                        current++;
-                      }
-                    });
-                  },
-                  child: const Text("다음")),
-              ElevatedButton(
-                  onPressed: () {
-                    setState(() {
-                      if (current > 0) {
-                        current--;
-                      }
-                    });
-                  },
-                  child: const Text("이전")),
-              ElevatedButton(
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => const SecondPage()));
-                  },
-                  child: SizedBox(
-                      width: 200,
-                      child: Row(
-                        children: const [
-                          Hero(tag: 'detail', child: Icon(Icons.cake))
-                        ],
-                      ))),
-              ElevatedButton(onPressed: () {
-                Navigator.of(context).push(MaterialPageRoute(builder: (context)=> const SliverPage()));
-              }, child: const Text("To Sliver Page"))
-            ],
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-        ));
+    return Container(
+      
+    );
   }
 }
